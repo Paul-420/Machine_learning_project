9750938b4bafcc22b87bbd7a3355f13c871db809
Linkaon <94857147+Linkaon@users.noreply.github.com>
2025-01-05 23:21:23 +0100

    delete error

--- test/test_app.py
+++ test/test_app.py
@@ -22,15 +22,3 @@ def test_get_images():
         assert "class_label" in data[0]
         assert "image" in data[0]
 
-def test_predict_valid_image():
-    with open("test_image.png", "rb") as image_file:  # Assurez-vous d'avoir une image pour les tests
-        response = client.post("/predict/", files={"file": ("test_image.png", image_file, "image/jpeg")})
-    assert response.status_code == 200
-    assert "prediction" in response.json()
-
-def test_predict_invalid_image():
-    with open("test_image.txt", "rb") as invalid_file:  # Fichier texte pour tester une image invalide
-        response = client.post("/predict/", files={"file": ("test_image.txt", invalid_file, "text/plain")})
-    assert response.status_code == 422  # Attendre un code d'erreur pour une mauvaise image
-    assert "error" in response.json()
-
